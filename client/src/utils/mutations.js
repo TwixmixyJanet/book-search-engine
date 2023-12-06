8def98da2605c46b349540305bbe3196efdd219a
Janet Webster <twixmixy@gmail.com>
2023-12-05 19:41:48 -0500

    Add saveBook mutation in mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -25,7 +25,22 @@ export const ADD_USER = gql`
 `;
 
 export const SAVE_BOOK = gql`
-mutation saveBook($)
+mutation saveBook($booData: BookInput!) {
+  saveBook(bookData: $bookData) {
+    _id
+    username
+    email
+    bookCount
+    savedBooks {
+      bookId
+      authors
+      description
+      title
+      image
+      link
+    }
+  }
+}
 `;
 
 export const REMOVE_BOOK = gql`
